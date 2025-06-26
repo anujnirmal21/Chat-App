f0a90d5f15c041b4610ea913a6bb82df94b6d068
Anuj Nirmal <anunirmal5648@gmail.com>
2025-06-26 16:49:26 +0530

    fix: Update MongoDB connection to use environment variable

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://anunirmal5648:w2vire2KGi2HmXnE@cluster0.gtgeatk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("MongoDB connection error:", error);
